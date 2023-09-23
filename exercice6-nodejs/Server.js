1cc045f130d8991438ddcae418223fa120b1d234
malek-labidi <labidimalek@esprit.tn>
2023-09-23 14:19:12 +0100

    route: /game/select/:year (Récupérer la liste des jeux avec une date supérieure à celle passée en paramètre)

--- exercice6-nodejs/Server.js
+++ exercice6-nodejs/Server.js
@@ -1,10 +1,10 @@
-
 import { createServer } from 'http';
 import fs from 'fs';
-
+import url from 'url';
 
 const hostname = '127.0.0.1'; // L'@ du serveur 
 const port = process.env.PORT || 9090; // Le port du serveur
+const cheminFichierJSON = 'SteamGames.json';
 
 /**
  * Créer un serveur HTTP
@@ -12,51 +12,61 @@ const port = process.env.PORT || 9090; // Le port du serveur
  * @param (ServerResponse) res
 */
 const server = createServer((req, res) => {
+    fs.readFile(cheminFichierJSON, 'utf8', (err, data) => {
+        const parsedUrl = url.parse(req.url, true);
+        const paths = parsedUrl.pathname.split('/').filter(Boolean);
 
-   
+        if (err) {
+            console.error('Erreur de lecture du fichier JSON :', err);
+            res.writeHead(500);
+            res.end('Internal Server Error');
+            return;
+        }
 
-        const cheminFichierJSON = 'SteamGames.json';
+        let jsonData;
+        try {
+            jsonData = JSON.parse(data);
+        } catch (parseErr) {
+            console.error('Erreur de parsing du JSON:', parseErr);
+            res.writeHead(500);
+            res.end('Internal Server Error');
+            return;
+        }
 
-        fs.readFile(cheminFichierJSON, 'utf8', (err, data) => {
-            if (err) {
-                console.error('Erreur de lecture du fichier JSON :', err);
-                res.statusCode = 500; // Erreur interne du serveur
-                res.end('Erreur de lecture du fichier JSON');
-                return;
-            }
-            if (req.url === '/game') {
-            res.statusCode = 200; // OK
-            res.end(data);
-            }
-            
-        });
-    
-if(req.url === '/')
-{
-     res.statusCode = 200; // 200 est le code de statut HTTP 
-        res.setHeader('Content-Type', 'text/plain'); // Configurer l'entete de la requête 
-        res.end('Hello World!'); // end signifie que l'on envoie la réponse au requérant
-}
+        /**
+         * Récupérer la liste de tous les jeux
+         *
+         *  */
 
+        if (paths[0] === 'game' && paths.length === 1) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(jsonData));
+            return;
+        }
+        
+        /**
+         * Récupérer la liste des jeux avec une date supérieure à celle passée en paramètre
+         * 
+         * */
+        if (paths[0] === 'game' && paths[1] === 'select' && paths[2] !== '') {
+            console.log(paths[2]);
+            var data_filter = jsonData.filter(e => e.Year > paths[2]);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(data_filter));
+            return;
+        }
+
+        res.writeHead(404);
+        res.end('Not Found');
+    });
 });
-
-
-
-
-
-
 
 
 /**
  *  Démarrer le serveur à l'écoute des connexions
  */
 
-
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 
 });
-
-
-
-
